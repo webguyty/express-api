aee0955613ac05614230ae9518e36c561bfa3edc
Tyler Austin <tyler@webguyty.com>
2020-12-24 16:30:45 -0800

    changed file names to server

--- routes/home.js
+++ routes/home.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  try {
-    res.send('This / get is working')
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error - Dangit');
-  }
+  res.send('hello world')
+  // try {
+  //   res.send('This / get is working')
+  // } catch (err) {
+  //   console.error(err.message);
+  //   res.status(500).send('Server Error - Dangit');
+  // }
 });
 
 
