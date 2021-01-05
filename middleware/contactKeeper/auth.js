1b644a0cad36a902e209e3e9e4f3eccab45a313d
Tyler Austin <tyler@webguyty.com>
2021-01-05 12:35:54 -0800

    fixed token validation

--- middleware/contactKeeper/auth.js
+++ middleware/contactKeeper/auth.js
@@ -12,7 +12,7 @@ module.exports = function (req, res, next) {
   }
 
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, config.get('jwtSecretCK'));
     req.user = decoded.user;
     next();
   } catch (error) {
