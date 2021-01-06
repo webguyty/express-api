336065d7c4a5e67b367a767b599d5f8effbd6375
Tyler Austin <tyler@webguyty.com>
2021-01-06 10:06:34 -0800

    updated auth pathing

--- routes/goalSeeker/auth.js
+++ routes/goalSeeker/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const auth = require('../middleware/auth');
+const auth = require('../../authmiddleware/auth');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
